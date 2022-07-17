ddf794e3f892e2f02c365bf12a78cceaf615e570
Se Yeong Lee <qerogram@naver.com>
2022-07-17 15:18:41 +0000

    feat: connect between route path and page component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,24 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import MainPage from './component/page/MainPage';
+import PostWritePage from './component/page/PostWritePage';
+import PostViewPage from './component/page/PostViewPage';
+import styled from 'styled-components';
+
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
 
 const App = (props) => {
     return (
         <BrowserRouter>
+            <MainTitleText>qero 테스트</MainTitleText>
             <Routes>
-                <Route path="/" element={<MainPage />} > </Route>
+                <Route path="/" element={<MainPage />} />
+                <Route path="post-write" element={<PostWritePage />} />
+                <Route path="post/:postId" element={<PostViewPage />} />
             </Routes>
         </BrowserRouter>
     )
