2dd2fe9c6a752f090b14cfdb6d266a9c9544e0eb
Se Yeong Lee <qerogram@naver.com>
2022-07-17 15:02:19 +0000

    feat: implementated Main Page

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import MainPage from './component/page/MainPage';
 
 const App = (props) => {
     return (
         <BrowserRouter>
             <Routes>
-                {/* Route props 
-                    => path = 여기는 URI 가 들어옴.
-                    => element = 함수 명이 들어옴, 근데그냥 함수명이 아니라 {<함수명 />}
-                */}
-                {/* <Route path="/" element={<Blocks />} > </Route> */}
+                <Route path="/" element={<MainPage />} > </Route>
             </Routes>
         </BrowserRouter>
     )
