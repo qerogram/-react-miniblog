fa7e996944e7b560f2fca39bcb489228efd29369
Se Yeong Lee <qerogram@naver.com>
2022-07-17 15:06:47 +0000

    feat: implementated Post Write Page

---
+++ src/component/page/PostWritePage.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px)px;
+    display: flex;
+    flex-direction: coliumn;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+const PostWritePage = (props) => {
+    const navigate = useNavigate();
+
+    const [ title, setTitle ] = useState("");
+    const [ content, setContent ] = useState("");
+
+    return (
+        <Wrapper>
+            <Container>
+                <TextInput
+                    height={20}
+                    value={title}
+                    onChange={(event) => {
+                        setTitle(event.target.value);
+                    }}
+                />
+
+                <TextInput
+                    height={480}
+                    value={content}
+                    onChange={(event) => {
+                        setContent(event.target.value);
+                    }}
+                />
+
+                <Button
+                    title="글 작성하기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default PostWritePage;
